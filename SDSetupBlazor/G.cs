53b1ee28308503729a3283c8345abb566d11f1c1
Jorge Verdugo <jorgeverdugoch@gmail.com>
2025-02-16 23:04:03 -0300

    feat: Preparing to dockerize

--- SDSetupBlazor/G.cs
+++ SDSetupBlazor/G.cs
@@ -16,7 +16,7 @@ namespace SDSetupBlazor
 {
     public static class G {
 
-        public static string hostname = "https://files.sdsetup.com";
+        public static string hostname = "http://0.0.0.0:5000";
 
         public static string packageset = "default24";
         public static string channel = "latest";
